9519d2f91da34bbe10fc6b9bc4433dac0ac56089
carlosroo <carlos.rodriguez40@alu.uclm.es>
2023-11-25 19:24:08 +0100

    constants

--- handlers/constants.go
+++ handlers/constants.go
@@ -3,13 +3,13 @@ package handlers
 import "SEGRED_API/models"
 
 const (
-	version = 0
-	dir_usuarios = "data"
-	secret_key = "secret_key"
+	version               = 0
+	dir_usuarios          = "data"
+	secret_key            = "secret_key"
 	token_expiration_time = 5
-	bbdd = "usuarios.db"
-	tokenErroneo = "Noppers"
-	
+	bbdd                  = "usuarios.db"
+	tokenErroneo          = ""
 )
+
 var jwtKey = []byte(secret_key)
 var usersDB models.UsersDB
