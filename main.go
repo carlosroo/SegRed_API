311d8bc05c3de139fe8ceee0613439083036e44c
carlosroak <carlos.rodriguez@astrokube.com>
2024-11-25 19:21:55 +0100

    refactor main

--- main.go
+++ main.go
@@ -10,25 +10,27 @@ import (
 )
 
 func main() {
+	certFile := "cert.pem"
+	keyFile := "key_unencrypted.pem"
 	router := mux.NewRouter().StrictSlash(true)
 
 	err := handlers.InitializeDatabase()
 	if err != nil {
-		println("Error al inicializar la base de datos:", err)
-		return
+		log.Fatalf("Error al inicializar la base de datos: %v", err)
 	}
 
+	RegisterRoutes(router)
+
+	log.Println("Servidor corriendo en el puerto 5000")
+	//log.Fatal(http.ListenAndServe(":5000", router))
+	log.Fatal(http.ListenAndServeTLS(":5000", certFile, keyFile, router))
+}
+
+func RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/", handlers.IndexRoute).Methods("GET")
 	router.HandleFunc("/version", handlers.GetVersion).Methods("GET")
 	router.HandleFunc("/signup", handlers.CreateUser).Methods("POST")
 	router.HandleFunc("/login", handlers.Login).Methods("POST")
 	router.HandleFunc("/{username}/_all_docs", handlers.GetAllFiles).Methods("GET")
 	router.HandleFunc("/{username}/{doc_id}", handlers.HandleFileOperations)
-
-	certFile := "cert.pem"
-	keyFile := "key_unencrypted.pem"
-
-	log.Println("Servidor corriendo en el puerto 5000")
-	//log.Fatal(http.ListenAndServe(":5000", router))
-	log.Fatal(http.ListenAndServeTLS(":5000", certFile, keyFile, router))
 }
